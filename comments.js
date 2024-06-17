3da573f4e120f34db4daa435206d80c878a4b6c0
vibhupuri <vibhupuri@users.noreply.github.com>
2024-06-17 09:53:54 +0000

    new file:   comments.js

---
+++ comments.js
@@ -0,0 +1,14 @@
+//Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//Create a route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+//Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
